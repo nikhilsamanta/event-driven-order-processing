91694526dc4f1b0eb92ba8b6a09dadd41c884587
nikhilsamanta <nikhilsamanta88@gmail.com>
2026-06-25 12:30:35 +0530

    odercode

--- lambdas/OrderReceiver.py
+++ lambdas/OrderReceiver.py
@@ -1,1 +1,28 @@
 
+import json
+import boto3
+
+sqs = boto3.client('sqs')
+
+QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/959222626247/order-processing.fifo'
+
+def lambda_handler(event, context):
+
+    if 'body' in event:
+        body = json.loads(event['body'])
+    else:
+        body = event
+
+    response = sqs.send_message(
+        QueueUrl=QUEUE_URL,
+        MessageBody=json.dumps(body),
+        MessageGroupId='orders'
+    )
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'message': 'Order queued successfully',
+            'messageId': response['MessageId']
+        })
+    }
